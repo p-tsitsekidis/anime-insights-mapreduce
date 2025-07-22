fef4e28ec492848fa9bbe281c9914f1e8adce503
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2025-07-23 00:19:33 +0300

    Fix incorrect filename in data loading and folder typo

---
+++ studio_ghibli_analysis/ghibli_ratings_over_time.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Plot Studio Ghibli anime ratings by release year.
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load data
+data = pd.read_csv('studio_ghibli_analysis/studio_ghibli_anime_metadata.csv')
+
+# Bar Plot
+plt.figure(figsize=(10, 6))
+plt.bar(data['Release_year'], data['Rating'])
+plt.title('Studio Ghibli Anime Ratings by Release Year')
+plt.xlabel('Release Year')
+plt.ylabel('Rating')
+plt.show()
