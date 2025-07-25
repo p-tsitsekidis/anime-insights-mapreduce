520c1d28c27057be508e84b708d502bbba7273eb
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2025-07-25 23:57:32 +0300

    Add script to visualize designer metadata by productions, studios, and rating

---
+++ designer_analysis/designer_analysis.py
@@ -0,0 +1,50 @@
+# !/usr/bin/env python3
+"""
+Analyze anime character designers based on total productions, number of studios collaborated with,
+and highest average rating. Generates three bar charts showing the top 30 designers by each metric.
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load data
+df = pd.read_json("/home/petros/Desktop/anime/task5/task5.json")
+
+# Getting the info from the nested studio
+df['Num_Studios'] = df['studios'].apply(len)
+df['Highest_Rating'] = df.apply(lambda x: max([studio['avgRating'] for studio in x['studios'] if studio.get('avgRating') is not None], default=None), axis=1)
+
+
+# top 30
+df_most_productions = df.nlargest(30, 'totalProductions')
+df_most_studios = df.nlargest(30, 'Num_Studios')
+df_highest_ratings = df.nlargest(30, 'Highest_Rating')
+
+# Designers with the most productions
+fig, ax = plt.subplots(figsize=(10, 7))
+ax.bar(df_most_productions['designer'], df_most_productions['totalProductions'], color='skyblue')
+ax.set_title('Top 30 Designers by Number of Productions')
+ax.set_ylabel('Total Productions')
+ax.set_xticklabels(df_most_productions['designer'], rotation=90)
+plt.tight_layout()
+plt.savefig("/home/petros/Desktop/anime/task5/most_productions.png")
+plt.close()
+
+# Designers that have worked with the most unique studios
+fig, ax = plt.subplots(figsize=(10, 7))
+ax.bar(df_most_studios['designer'], df_most_studios['Num_Studios'], color='lightgreen')
+ax.set_title('Top 30 Designers by Number of Studios Worked With')
+ax.set_ylabel('Number of Studios')
+ax.set_xticklabels(df_most_studios['designer'], rotation=90)
+plt.tight_layout()
+plt.savefig("/home/petros/Desktop/anime/task5/most_studios.png")
+plt.close()
+
+# Designers with the highest ratings
+fig, ax = plt.subplots(figsize=(10, 7))
+ax.bar(df_highest_ratings['designer'], df_highest_ratings['Highest_Rating'], color='salmon')
+ax.set_title('Top 30 Designers by Highest Rating')
+ax.set_ylabel('Highest Average Rating')
+ax.set_xticklabels(df_highest_ratings['designer'], rotation=90)
+plt.tight_layout()
+plt.savefig("/home/petros/Desktop/anime/task5/highest_ratings.png")
+plt.close()
